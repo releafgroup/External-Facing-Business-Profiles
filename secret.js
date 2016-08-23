5add461bf686ae25040883929ca1dad5962a1fbb
EmmanuelAzuh <emazuh@mit.edu>
2016-08-23 01:31:30 -0400

    Added secret file and updated package.json

---
+++ secret.js
@@ -0,0 +1,18 @@
+exports.secret = {
+  linkedIn : {
+    id : '', //developer appId
+    secret: '', //developer appSecret
+    inEmail: '', //linkedIn signin email
+    inPassword: '' //linkedIn signin password
+  },
+  facebook: {
+    id: '', //developer appId
+    secret: '', //developer appSecret
+    fbEmail: '', //facebook signin email
+    fbPassword: '' //facebook signin password
+  },
+  gmail: {
+	email: '', //gmail signin address
+    password: '' //gmail signin password
+  }
+};
