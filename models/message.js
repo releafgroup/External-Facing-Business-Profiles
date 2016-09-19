117b32352bbea357e8cdb8bbadd6fa4586f7dfc5
Adegoke Obasa <adegokeobasa@gmail.com>
2016-09-19 18:40:06 +0100

    (Cleanup): Reformat message model

--- models/message.js
+++ models/message.js
@@ -1,25 +1,20 @@
 var mongoose = require('mongoose'),
-
-Schema = mongoose.Schema;
-
-
+    Schema = mongoose.Schema;
 
 // create a schema for massage
 var MessageSchema = new Schema({
-  username: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
-  room: {type: String, required: true},
-  content: {type: String},
-  type: {type: String, default: "group", enum: ['group', 'private']},
-  to: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},//private(direct-user to user)//group,
-  file :{data:Buffer, contentType:String, name:String}// could be BSON to store 16mb data to db directlly //need to investigate
+    username: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
+    room: {type: String, required: true},
+    content: {type: String},
+    type: {type: String, default: "group", enum: ['group', 'private']},
+    to: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},//private(direct-user to user)
+    file: {data: Buffer, contentType: String, name: String}// TODO could be BSON to store 16mb data to db directly
 }, {
-  timestamps: true
+    timestamps: true
 });
 
 // create a model from the message schema
 var Message = mongoose.model('Message', MessageSchema);
 
 //we will add schema for groups later on
-
-
 module.exports = Message;
