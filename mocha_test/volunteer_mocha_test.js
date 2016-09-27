cc5709766549ea0168818ef7b4c106330de0cbe2
Adegoke Obasa <adegokeobasa@gmail.com>
2016-09-27 08:23:08 +0100

    (Media Upload): Write test for resume and profile_photo upload

--- mocha_test/volunteer_mocha_test.js
+++ mocha_test/volunteer_mocha_test.js
@@ -1,10 +1,12 @@
 var request = require('supertest');
 var mongoose = require('mongoose');
+var should = require('should');
 var config = require('../config.js');
 var url = process.env.HOST_DOMAIN || 'http://localhost:3000';
 var testHelpers = require('../helpers/test');
+var faker = require('faker');
 
-var super_agent = request.agent(url);
+var superAgent = request.agent(url);
 
 var user1 = testHelpers.user1;
 var user1Id = -1;
@@ -36,7 +38,7 @@ var company2 = JSON.parse(JSON.stringify(company1));
 
 // TODO: add test cases for user permissions
 // i.e. companies can't access this, users can't access other users data, etc.
-describe('Routing', function () {
+describe('Volunteer Test Cases', function () {
     before(function (done) {
         // Use mocha test db
         mongoose.connect(config.mocha_database, function () {
@@ -61,7 +63,7 @@ describe('Routing', function () {
     describe('Volunteer Sign Up', function () {
         it('tests basic sign up of Volunteer', function (done) {
 
-            super_agent
+            superAgent
                 .post('/users/auth/signup')
                 .send(user1)
                 .expect(200) //Status code
@@ -74,7 +76,7 @@ describe('Routing', function () {
         });
 
         it('tests if user is logged in after sign up', function (done) {
-            super_agent
+            superAgent
                 .get('/users/')
                 .expect(200)
                 .end(function (err, res) {
@@ -85,12 +87,12 @@ describe('Routing', function () {
         });
 
         it('tests user log out and that user cant access routes after log out', function (done) {
-            super_agent
+            superAgent
                 .get('/users/auth/logout')
                 .expect(200)
                 .end(function (err, res) {
                     res.body.success.should.equal(true);
-                    super_agent
+                    superAgent
                         .get('/users/')
                         .end(function (err, res) {
                             res.body.success.should.not.equal(true);
@@ -102,13 +104,13 @@ describe('Routing', function () {
         });
 
         it('tests logging in user and accessing a route', function (done) {
-            super_agent
+            superAgent
                 .post('/users/auth/login')
                 .expect(200)
                 .send(user1)
                 .end(function (err, res) {
                     res.body.success.should.equal(true);
-                    super_agent
+                    superAgent
                         .get('/users/')
                         .end(function (err, res) {
                             res.body.success.should.equal(true);
@@ -120,7 +122,7 @@ describe('Routing', function () {
 
 
         it('tests updating password and all other extra information after initial account creation', function (done) {
-            super_agent
+            superAgent
                 .put('/users/')
                 .send(userUpdateInfo)
                 .end(function (err2, res2) {
@@ -129,6 +131,31 @@ describe('Routing', function () {
                 });
         });
 
+
+        it('tests signup with profile_data and resume', function (done) {
+            user1['local.email'] = faker.internet.email();
+            user1.profile_photo_data = '123456789';
+            user1.resume_data = '123455789';
+            user1.resume_extension = 'docx';
+
+            var agent = request.agent(url);
+            agent.post('/users/auth/signup')
+                .send(user1)
+                .expect(200) //Status code
+                .end(function (err, res) {
+                    user1Id = res.body.message;
+                    res.body.success.should.equal(true);
+
+                    agent.get('/users')
+                        .end(function (getErr, getRes) {
+                            getRes.body.success.should.equal(true);
+                            should.exist(getRes.body.message.profile_photo);
+                            should.exist(getRes.body.message.resume);
+                            getRes.body.message.resume.should.endWith(user1.resume_extension);
+                            done();
+                        });
+                });
+        });
     });
 
 
@@ -138,7 +165,7 @@ describe('Routing', function () {
         it('tests that we check if an email has valid format', function (done) {
 
 
-            super_agent
+            superAgent
                 .post('/users/auth/signup')
                 .send(userWithBadEmail)
                 .expect(200)
@@ -153,7 +180,7 @@ describe('Routing', function () {
             var userAboveMaxAge = JSON.parse(JSON.stringify(user1));
             userAboveMaxAge['dob'] = '1936-09-23'; // always above max age
             userAboveMaxAge['local.email'] = 'volunteer@old.com';
-            super_agent
+            superAgent
                 .post('/users/auth/signup')
                 .send(userAboveMaxAge)
                 .expect(400) //Status code
@@ -167,7 +194,7 @@ describe('Routing', function () {
             var userWithinAgeLimit = JSON.parse(JSON.stringify(user1));
             userWithinAgeLimit['dob'] = '2001-09-22';
             userWithinAgeLimit['local.email'] = 'volunteer@rightage.com';
-            super_agent
+            superAgent
                 .post('/users/auth/signup')
                 .send(userWithinAgeLimit)
                 .expect(200) //Status code
@@ -184,7 +211,7 @@ describe('Routing', function () {
                 currentDate.getFullYear() - 15, currentDate.getMonth(), currentDate.getDate()
             );
             userBelowMinAge['local.email'] = 'volunteer@younger.com';
-            super_agent
+            superAgent
                 .post('/users/auth/signup')
                 .send(userBelowMinAge)
                 .expect(400) //Status code
@@ -225,7 +252,7 @@ describe('Routing', function () {
                 "skill_3_rating": 3,
                 "gender": "Female",
                 "dob": "2016-06-07"
-            }
+            };
 
             request(url)
                 .post('/users')
@@ -239,7 +266,7 @@ describe('Routing', function () {
 
 
         it('tests that volunteer cannot change id', function (done) {
-            super_agent
+            superAgent
                 .put('/users/')
                 .send(userWithBadId)
                 .end(function (err2, res2) {
@@ -250,7 +277,7 @@ describe('Routing', function () {
 
 
         it('tests that volunteer cannot change email', function (done) {
-            super_agent
+            superAgent
                 .put('/users/')
                 .send(userWithBadEmail)
                 .end(function (err2, res2) {
@@ -363,7 +390,7 @@ describe('Routing', function () {
 
         describe('getting project info', function () {
             it('checks retrieval of list of projects', function (done) {
-                super_agent
+                superAgent
                     .get('/users/projects')
                     .end(function (err, res) {
                         res.body.success.should.equal(true);
@@ -373,7 +400,7 @@ describe('Routing', function () {
             });
 
             it('checks retrieval of single project', function (done) {
-                super_agent
+                superAgent
                     .get('/users/project/' + project1Id)
                     .end(function (err, res) {
                         res.body.success.should.equal(true);
@@ -383,7 +410,7 @@ describe('Routing', function () {
 
 
             it('checks retrieval of all of a companys projects', function (done) {
-                super_agent
+                superAgent
                     .get('/users/company/' + company1Id + '/projects')
                     .end(function (err, res) {
                         res.body.success.should.equal(true);
@@ -399,12 +426,12 @@ describe('Routing', function () {
         describe('favoriting a project', function () {
 
             it('should cause user 1 to have favorite column with proj_id', function (done) {
-                super_agent
+                superAgent
                     .put('/users/projects/favorite/' + project1Id)
                     .expect(200)
                     .end(function (err, res) {
                         if (res.body.success) {
-                            super_agent
+                            superAgent
                                 .get('/users')
                                 .expect(200)
                                 .end(function (err2, res2) {
@@ -418,12 +445,12 @@ describe('Routing', function () {
             });
 
             it('should switch between favoriting project 2 instead of project 1', function (done) {
-                super_agent
+                superAgent
                     .put('/users/projects/favorite/' + project2Id)
                     .expect(200)
                     .end(function (err, res) {
                         if (res.body.success) {
-                            super_agent
+                            superAgent
                                 .get('/users')
                                 .expect(200)
                                 .end(function (err2, res2) {
@@ -437,12 +464,12 @@ describe('Routing', function () {
             });
 
             it('makes user de-favorite project 2', function (done) {
-                super_agent
+                superAgent
                     .put('/users/projects/favorite/' + project2Id)
                     .expect(200)
                     .end(function (err, res) {
                         if (res.body.success) {
-                            super_agent
+                            superAgent
                                 .get('/users')
                                 .expect(200)
                                 .end(function (err2, res2) {
