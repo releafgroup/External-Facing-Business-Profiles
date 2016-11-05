8acf13edf72e8094029ab048b5841d36914afb7e
Adegoke Obasa <adegokeobasa@gmail.com>
2016-11-05 22:59:01 +0100

    (Configurable Fields): Use config to get MongoDB URI

--- utils/db.js
+++ utils/db.js
@@ -1,6 +1,6 @@
+var config = require('../config.js');
 var MongoClient = require('mongodb').MongoClient;
-var URI = process.env.MONGODB_URI || 'mongodb://localhost/first_test_db';
 
 module.exports = function execute(callback) {
-    MongoClient.connect(URI, callback);
+    MongoClient.connect(config.database, callback);
 };
