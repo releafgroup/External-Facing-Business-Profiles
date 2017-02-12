6007fd53f176e8701bc4fbc144a4ba767e571f02
Adeyemi Olaoye <yemi@cottacush.com>
2017-02-13 00:28:49 +0100

    (R-Factor Calculation) Change R-factor to use factor weights and weighted score only

--- controllers/company.js
+++ controllers/company.js
@@ -11,20 +11,25 @@ module.exports = {
                 for (var i = 0; i < companyInputs.length; i++) {
                     var companySubFactors = companyInputs[i].toObject();
                     let rScore = 0;
+                    let stats = {};
                     for (var j = 0; j < subFactors.length; j++) {
                         var subFactor = subFactors[j];
                         var companySubFactor = companySubFactors[subFactor.sub_factor];
-                        const weight = requestParams[subFactor.factor] || subFactor.weight;
+                        const weight = Number(requestParams[subFactor.factor]) || (1 / subFactors.length);
                         if (companySubFactor) {
                             const scoreRating = subFactor['score_' + companySubFactor.score + '_rating'];
-                            const weightedScore = Number(weight) * Number(scoreRating);
-                            rScore += weightedScore;
-                            companySubFactors[subFactor.sub_factor].weighted_score = weightedScore;
+                            rScore += Number(weight) * companySubFactors[subFactor.sub_factor].weighted_score;
                             companySubFactors[subFactor.sub_factor].score_rating = scoreRating;
-                            companySubFactors[subFactor.sub_factor].weight = weight;
+
+                            if (typeof stats[subFactor.factor] == 'undefined') {
+                                stats[subFactor.factor] = 0;
+                            }
+
+                            stats[subFactor.factor] += companySubFactors[subFactor.sub_factor].weighted_score;
                         }
                     }
                     companySubFactors.r_score = Math.floor(rScore);
+                    companySubFactors.stats = stats;
                     companies.push(companySubFactors);
                 }
 
@@ -34,6 +39,7 @@ module.exports = {
 
                 var limit = requestParams.limit || ((companies.length > 5) ? 5 : companies.length);
                 companies = (companies.length > 1) ? companies.slice(0, limit - 1) : companies;
+                console.log(companies);
                 return jsendRepsonse.sendSuccess(companies, res);
             });
         });
--- tests/acceptance/company_test.js
+++ tests/acceptance/company_test.js
@@ -4,9 +4,9 @@ const config = require('../../config/config');
 
 const URL = config.tests.TEST_URL;
 
-describe('Factors', function () {
+describe('Company', function () {
     it('Test Get Companies', (done) => {
-        request(URL).get('/companies?business_model=0.5')
+        request(URL).get('/companies?business_model=0.009708737864077669&growth_plan=0.019417475728155338&legal_factor=0.1941747572815534&financial_factor=0.1941747572815534&market_factor=0.1941747572815534&team_factor=0.1941747572815534&social_factor=0.1941747572815534')
             .expect(200)
             .end(function (err, res) {
                 res.body.status.should.equal('success');
