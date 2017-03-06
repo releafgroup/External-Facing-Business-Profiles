cae87c43d0b32b0227f982092b95f6d4d7dcdc90
Adegoke Obasa <adegokeobasa@gmail.com>
2017-03-07 00:29:19 +0100

    (Factor Queries): Add acceptance tests for Get Factor Queries API

--- models/factor_query.js
+++ models/factor_query.js
@@ -8,5 +8,4 @@ var FactorQuerySchema = new Schema({
     "weight": {type: Number, required: true},
 }, {collection: 'factor_queries'});
 
-
 module.exports = mongoose.model('FactorQuerySchema', FactorQuerySchema);
