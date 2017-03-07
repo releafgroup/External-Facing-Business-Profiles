dc8c0035e4e0f6ae1bf10039083f0f7daf618eef
Adegoke Obasa <adegokeobasa@gmail.com>
2017-03-07 01:07:25 +0100

    (Factor Queries): WIP - Sending of mails to business as per factor query

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 /**
  * Load environment variables from .env file, where API keys and passwords are configured.
  */
-dotenv.load({path: __dirname + '/../env/.env'});
+dotenv.load({path: __dirname + '/env/.env'});
 
 module.exports = Object.freeze({
     APPLICATION_ENV: process.env.APPLICATION_ENV,
--- controllers/factor.js
+++ controllers/factor.js
@@ -1,6 +1,7 @@
 const jsendResponse = require('../helpers/jsend_response');
 const SubFactor = require('../models/sub_factor');
 const FactorQuery = require('../models/factor_query');
+const nodeMailer = require('../libs/node_mailer');
 
 module.exports = {
     getAll: (req, res) => {
@@ -33,6 +34,26 @@ module.exports = {
                 jsendResponse.sendError("Query not found", 404, res);
             } else {
                 jsendResponse.sendSuccess(query, res);
+            }
+        }).catch((error) => {
+            jsendResponse.sendError(error.message, 500, res);
+        });
+    },
+    sendQueryEmail: (req, res) => {
+        FactorQuery.findOne({_id: req.params.id}).then((query) => {
+            if (!query) {
+                jsendResponse.sendError("Query not found", 404, res);
+            } else {
+                const businessName = 'Fresh Direct';
+                const investorName = 'Goke Obasa';
+                const investorCompany = 'Gokizzle and Sons';
+                const businessEmail = 'goke@mailinator.com';
+                const html = `Hi ${businessName},<br/> My name is ${investorName} from ${investorCompany}.<br/>
+                I'm interested in learning more about your business could you ${query.content} 
+                Thank you , looking forward to hearing back.`;
+
+                nodeMailer.send('Hello There from Releaf', html, businessEmail, [], ['releaffounders@mit.edu'], 'data@releaf.ng');
+                jsendResponse.sendSuccess(true, res);
             }
         }).catch((error) => {
             jsendResponse.sendError(error.message, 500, res);
