0c2d67895982f735370dff4285a87505eadc9ab7
Adegoke Obasa <adegokeobasa@gmail.com>
2017-03-07 01:24:09 +0100

    (Factor Queries): Add validations to send email for factor query route

--- controllers/factor.js
+++ controllers/factor.js
@@ -2,6 +2,7 @@ const jsendResponse = require('../helpers/jsend_response');
 const SubFactor = require('../models/sub_factor');
 const FactorQuery = require('../models/factor_query');
 const nodeMailer = require('../libs/node_mailer');
+const sendFactorQueryEmailValidation = require('../validations/send_factor_query_email_validation');
 
 module.exports = {
     getAll: (req, res) => {
@@ -40,23 +41,36 @@ module.exports = {
         });
     },
     sendQueryEmail: (req, res) => {
-        FactorQuery.findOne({_id: req.params.id}).then((query) => {
-            if (!query) {
-                jsendResponse.sendError("Query not found", 404, res);
+        sendFactorQueryEmailValidation(req.body, (err, value) => {
+            if (err) {
+                jsendResponse.sendError(err.message, 400, res);
             } else {
-                const businessName = 'Fresh Direct';
-                const investorName = 'Goke Obasa';
-                const investorCompany = 'Gokizzle and Sons';
-                const businessEmail = 'goke@mailinator.com';
-                const html = `Hi ${businessName},<br/> My name is ${investorName} from ${investorCompany}.<br/>
-                I'm interested in learning more about your business could you ${query.content} 
+                FactorQuery.findOne({_id: req.params.id}).then((query) => {
+                    if (!query) {
+                        jsendResponse.sendError("Query not found", 404, res);
+                    } else {
+                        const businessName = req.body.business_name;
+                        const investorName = req.body.investor_name;
+                        const investorCompany = req.body.investor_company;
+                        const businessEmail = req.body.business_email;
+                        const html = `Hi ${businessName},<br/> My name is ${investorName} from ${investorCompany}.<br/>
+                I'm interested in learning more about your business could you ${query.content} <br/>
                 Thank you , looking forward to hearing back.`;
 
-                nodeMailer.send('Hello There from Releaf', html, businessEmail, [], ['releaffounders@mit.edu'], 'data@releaf.ng');
-                jsendResponse.sendSuccess(true, res);
+                        nodeMailer.send(
+                            'Hello There from Releaf',
+                            html,
+                            businessEmail,
+                            [],
+                            ['releaffounders@mit.edu'],
+                            'data@releaf.ng'
+                        );
+                        jsendResponse.sendSuccess(true, res);
+                    }
+                }).catch((error) => {
+                    jsendResponse.sendError(error.message, 500, res);
+                });
             }
-        }).catch((error) => {
-            jsendResponse.sendError(error.message, 500, res);
         });
     }
 };
--- libs/node_mailer.js
+++ libs/node_mailer.js
@@ -1,4 +1,4 @@
-var config = require('./../config');
+var config = require('./../config/config');
 var nodeMailer = require('nodemailer');
 // create reusable transporter object using SMTP transport
 
