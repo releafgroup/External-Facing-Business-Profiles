77ee0be320da194f0cbc7cbadd5ac7796dec0414
Clive Mudanda <clive@Clives-MacBook-Air.local>
2017-05-05 16:45:57 -0400

    convert money ready - changed to pull Rates from yahoo api as that is free

--- controllers/company.js
+++ controllers/company.js
@@ -2,16 +2,15 @@ const jsendRepsonse = require('../helpers/jsend_response');
 const SubFactor = require('../models/sub_factor');
 const Company = require('../models/company');
 const config = require('../config/config');
-const rates = require('open-exchange-rates');
-const money = require('money');
-rates.set({ app_id: 'd91f500a5b494c4a98b1f6c39fee47a0' })  //Temporarily putting app ID(ope exchange rates) here but will move to .env after testing
-
+const currency = require('y-currency');
 module.exports = {
-    getAll: (req, res) => {
+	getAll: (req, res) => {
         var requestParams = req.query;
         var companies = [];
+        var start_index = Number(requestParams.start_index) || 0;
+        var limit = Number(requestParams.limit) || 6;
         SubFactor.find().then((subFactors) => {
-            Company.find().then((companyInputs) => {
+            Company.find().skip(start_index).limit(limit).then((companyInputs) => {
                 for (var i = 0; i < companyInputs.length; i++) {
                     var companySubFactors = companyInputs[i].toObject();
 
@@ -42,118 +41,103 @@ module.exports = {
                     return parseFloat(b.r_score) - parseFloat(a.r_score);
                 });
 
-                var limit = requestParams.limit || ((companies.length > 6) ? 6 : companies.length);
-                companies = (companies.length > 1) ? companies.slice(0, limit) : companies;
                 return jsendRepsonse.sendSuccess(companies, res);
             });
         });
     },
-
-    get: (req, res) => {
-        if (!req.params.id) {
-            return jsendRepsonse.sendError('Company not found', 404, res);
-        }
-
-        Company.findById(req.params.id, function (err, company) {
-            if (!company) {
-                return jsendRepsonse.sendError('Company not found', 404, res);
-            }
-
-            company = company.toObject();
-            let companyAvailableSubFactors = 0;
-            let totalSubFactors = 0;
-            SubFactor.find().then((subFactors) => {
-                totalSubFactors = subFactors.length;
-                subFactors.forEach((subFactor) => {
-                    let current_score = Number(company.hasOwnProperty(subFactor.sub_factor));
-                    if(current_score != -1){
-                        companyAvailableSubFactors += current_score;
-                    }
-                });
-                company.profile_completion_level = parseFloat((companyAvailableSubFactors / totalSubFactors) * 100).toFixed(2);
-                return jsendRepsonse.sendSuccess(company, res);
-            });
-        });
-    },
-
-    search: (req, res) => {
-        let query = req.query;
-        let sort = {};
-        let sortKey = query.sort_by || false;
-        if (sortKey) {
-            sort[sortKey] = -1;
-        }
-
-        let size = query.size || config.QUERY_LIMIT;
-        let page = query.page || 1;
-
-        let userQuery = {};
-
-        SubFactor.find().distinct('factor').then(function (factors) {
-            for (let key in query) {
-                if (["size", "sort_by", "page", "token"].indexOf(key) >= 0) {
-                    continue;
-                }
-
-                if (query.hasOwnProperty(key) && factors.indexOf(key) == -1) {
-                    // Other business property
-                    userQuery[key] = {'$regex': '.*' + query[key] + '.*', '$options': 'i'};
-                } else if (factors.indexOf(key) > -1) {
-                    // Filtering by range of r-factor scores
-                    const minMaxQuery = query[key].split(',');
-                    if (minMaxQuery.length == 2) {
-                        const minQuery = minMaxQuery[0];
-                        const maxQuery = minMaxQuery[1];
-                        userQuery[key] = {};
-                        if (minQuery.length > 0) {
-                            userQuery[key]['$gte'] = Number(minQuery);
-                        }
-                        if (maxQuery.length > 0) {
-                            userQuery[key]['$lte'] = Number(maxQuery);
-                        }
-                    }
-                }
-            }
-
-            Company.count(userQuery, function (err, total) {
-                Company.find(userQuery).sort(sort).limit(parseInt(size)).skip((page - 1) * size).exec(function (err, company) {
-                    if (!company) {
-                        return jsendRepsonse.sendError('Error occured', 400, res);
-                    }
-
-                    let result = {
-                        result: company,
-                        total: total,
-                        page: page,
-                        size: size,
-                    };
-                    return jsendRepsonse.sendSuccess(result, res);
-
-                });
-            });
-        });
-
-    },
-    
-    convertMoney: (req, res) => {
-        var requestParams = req.query;
-        var value = 0;		
-
-		rates.latest(function() {
-		money.rates = rates.rates;
-		money.base = rates.base;
-	
-		// USD to Naira
-		if(requestParams.from == 'USD'){
-		value = money(Number(requestParams.value)).from('USD').to('NGN');
-		}else{		
-		//Naira to USD
-		value = money(Number(requestParams.value)).from('NGN').to('USD');
+	get: (req, res) => {
+		if (!req.params.id) {
+			return jsendRepsonse.sendError('Company not found', 404, res);
 		}
-		return jsendRepsonse.sendSuccess(value, res);
-});
-      
-    },
-
-
+		Company.findById(req.params.id, function(err, company) {
+			if (!company) {
+				return jsendRepsonse.sendError('Company not found', 404, res);
+			}
+			company = company.toObject();
+			let companyAvailableSubFactors = 0;
+			let totalSubFactors = 0;
+			SubFactor.find().then((subFactors) => {
+				totalSubFactors = subFactors.length;
+				subFactors.forEach((subFactor) => {
+					let current_score = Number(company.hasOwnProperty(subFactor.sub_factor));
+					if (current_score != -1) {
+						companyAvailableSubFactors += current_score;
+					}
+				});
+				company.profile_completion_level = parseFloat((companyAvailableSubFactors / totalSubFactors) * 100).toFixed(2);
+				return jsendRepsonse.sendSuccess(company, res);
+			});
+		});
+	},
+	search: (req, res) => {
+		let query = req.query;
+		let sort = {};
+		let sortKey = query.sort_by || false;
+		if (sortKey) {
+			sort[sortKey] = -1;
+		}
+		let size = query.size || config.QUERY_LIMIT;
+		let page = query.page || 1;
+		let userQuery = {};
+		SubFactor.find().distinct('factor').then(function(factors) {
+			for (let key in query) {
+				if (["size", "sort_by", "page", "token"].indexOf(key) >= 0) {
+					continue;
+				}
+				if (query.hasOwnProperty(key) && factors.indexOf(key) == -1) {
+					// Other business property
+					userQuery[key] = {
+						'$regex': '.*' + query[key] + '.*',
+						'$options': 'i'
+					};
+				} else if (factors.indexOf(key) > -1) {
+					// Filtering by range of r-factor scores
+					const minMaxQuery = query[key].split(',');
+					if (minMaxQuery.length == 2) {
+						const minQuery = minMaxQuery[0];
+						const maxQuery = minMaxQuery[1];
+						userQuery[key] = {};
+						if (minQuery.length > 0) {
+							userQuery[key]['$gte'] = Number(minQuery);
+						}
+						if (maxQuery.length > 0) {
+							userQuery[key]['$lte'] = Number(maxQuery);
+						}
+					}
+				}
+			}
+			Company.count(userQuery, function(err, total) {
+				Company.find(userQuery).sort(sort).limit(parseInt(size)).skip((page - 1) * size).exec(function(err, company) {
+					if (!company) {
+						return jsendRepsonse.sendError('Error occured', 400, res);
+					}
+					let result = {
+						result: company,
+						total: total,
+						page: page,
+						size: size,
+					};
+					return jsendRepsonse.sendSuccess(result, res);
+				});
+			});
+		});
+	},
+	convertMoney: (req, res) => {
+		var requestParams = req.query;
+		var value = Number(requestParams.value);
+		var to = '';
+		var from = requestParams.from;
+		if (from == 'USD') {
+			to = 'NGN';
+		} else {
+			to = 'USD';
+		}
+		currency.convert(value, from, to, function(err, converted) {
+			if (err) {
+				throw err;
+			} else {
+				return jsendRepsonse.sendSuccess(converted, res);
+			}
+		});
+	}
 };
