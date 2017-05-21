a80624e36c12c34cb7df7dae1be19eec9404dfba
Adegoke Obasa <adegokeobasa@gmail.com>
2017-05-21 01:25:29 +0100

    (Test): Move currency conversion test to `currency_test.js`

--- tests/acceptance/company_test.js
+++ tests/acceptance/company_test.js
@@ -15,27 +15,15 @@ describe('Company', function () {
             });
     });
 
-     it('Test Money Conversion', (done) => {
-        request(URL).get('/companies/convertMoney?value=10000&from=NGN')
+    it('Test Search Companies', (done) => {
+        request(URL)
+            .get('/companies/search')
             .set('x-access-token', config.ADMIN_SECRET_KEY)
             .expect(200)
             .end(function (err, res) {
-               res.body.status.should.equal('success');
+                //  console.log(res.body);
+                res.body.status.should.equal('success');
                 done();
             });
-    });
-
-
-    it('Test Search Companies',(done) => {
-        request(URL)
-        .get('/companies/search')
-        .set('x-access-token', config.ADMIN_SECRET_KEY)
-        .expect(200)
-        .end(function (err, res) {
-          //  console.log(res.body);
-            res.body.status.should.equal('success');
-            done();
-        });
     })
-
 });
