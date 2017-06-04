cd725f1890e23934751f85ae998717117e1d1e98
Adegoke Obasa <adegokeobasa@gmail.com>
2017-06-05 00:08:19 +0100

    feat(pagination): Rework pagination

--- controllers/company.js
+++ controllers/company.js
@@ -19,7 +19,8 @@ module.exports = {
                 return jsendResponse.sendSuccess(count, res);
             });
         }
-        var start_index = Number(requestParams.start_index) || 0;
+
+        let startIndex = Number(requestParams.start_index) || 0;
         SubFactor.find().then((subFactors) => {
             Company.find().then((companyInputs) => {
                 for (let i = 0; i < companyInputs.length; i++) {
@@ -38,7 +39,7 @@ module.exports = {
                             rScore += weight * weightedScore;
                             companySubFactors[subFactor.sub_factor].score_rating = scoreRating;
 
-                            if (typeof stats[subFactor.factor] == 'undefined') {
+                            if (typeof stats[subFactor.factor] === 'undefined') {
                                 stats[subFactor.factor] = 0;
                             }
 
@@ -55,7 +56,7 @@ module.exports = {
                 });
 
                 let limit = requestParams.limit || ((companies.length > 6) ? 6 : companies.length);
-                companies = (companies.length > 1) ? companies.slice(0, limit) : companies;
+                companies = (companies.length > 1) ? companies.slice(startIndex, startIndex + limit) : companies;
                 return jsendResponse.sendSuccess(companies, res);
             });
         });
