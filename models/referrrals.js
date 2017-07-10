d0b8364da2e30ccaf235f58c243565b75ccd11dc
dejijaye <dejijaye@gmail.com>
2017-07-09 18:02:12 -0700

    add phone number to referral schema and change required fields

--- models/referrrals.js
+++ models/referrrals.js
@@ -14,9 +14,10 @@ const ReferralSchema = new Schema({
         ref: 'BusinessOwner',
         required: true,
     },
-    email: {type: mongoose.SchemaTypes.Email, required: true},
+    email: {type: mongoose.SchemaTypes.Email},
     recipient_name: {type: String, required: true},
-    message: {type: String, required: true},
+    phone_number: {type: String, required: true},
+    message: String,
     created_at: {type: Date, default: Date.now}
 }, {collection: 'referrals'});
 
