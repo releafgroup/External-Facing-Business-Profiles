1985b44df75fb199cae506380d793006824a7bf1
calmest <okaforgreat@gmail.com>
2017-07-17 16:45:52 +0100

    last commit

---
+++ facingprofile/pages/company/_id.vue
@@ -0,0 +1,278 @@
+<template>
+  <section>
+      <div <div class="path0_fill">
+      <div class="row">
+        <div class="col-xs-12 col-md-12 col-sm-12"></br>
+      
+          <a class="logo-brand" href="#"><img src="~assets/images/releaf.png" alt="releaf logo"></a>
+        </div>
+          <div class="path3_fill col-xs-2 col-md-2 col-sm-2">
+            <img src="~assets/images/Wikimedia.png" alt="company logo">
+          </div>
+          <div class="path2_fill col-xs-10 col-md-10 col-sm-10">
+            <h3>PAYMENT CENTRIC</h3>
+
+          </div>
+
+          <div class="path4_fill col-xs-10 col-md-10 col-sm-10">
+
+            <center><p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            </p></center>
+          </div>
+          
+          <div class="row path11_fill col-xs-12 col-md-12 col-sm-12">
+            <center><a href="#"><img src="~assets/images/learn.png" alt="Learn more about us"></a></center>
+          </div>
+      </div>
+
+          <div class="row path7_fill">
+            
+            <div class="path10_fill col-xs-12 col-md-4 col-sm-12">
+
+              <img src="~assets/images/desc.svg">
+              <h3>Short Description</h3>
+              <p>
+                Payment Centric is a small business in the payment sector located in Nigeria with 10-50 full time employees.
+              </p>
+
+            </div>
+
+            <div class="path10_fill col-md-2">
+                
+            </div>
+
+            <div class="path10_fill col-xs-6 col-md-3 col-sm-6">
+                
+              <img src="~assets/images/country.svg">
+              <h3>Country</h3>
+              <p>Nigeria</p>
+
+              </br>
+
+              <img src="~assets/images/revenue.svg">
+              <h3>Annual Revenue</h3>
+              <p>$ 500, 000</p>
+
+            </div>
+
+            <div class="path10_fill col-xs-6 col-md-3 col-sm-6">
+                
+              <img src="~assets/images/industry.svg">
+              <h3>Industry</h3>
+              <p>Technology</p>
+
+              </br>
+
+              <img src="~assets/images/employee.svg">
+              <h3>Employee Size</h3>
+              <p>10 - 50</p>
+            </div>
+
+          </div>
+          
+          <div class="row">
+
+            <div class="head_text col-xs-12 col-md-12 col-sm-12">
+              
+              <h2>MORE DETAILS</h2>
+
+            </div>
+
+            <div class="body_text col-md-offset-1">
+              
+              <div class="col-xs-offset-1 col-xs-10  col-md-6 col-sm-offset-1 col-sm-10">
+                
+                <h3 style="color: #7f8c8d;">Founder</h3>
+                <p style="color: #7f8c8d;">Graham Glimour</p>
+
+                </br>
+
+                <h3 style="color: #7f8c8d;">Management Team</h3>
+                <p style="color: #7f8c8d; font-size: 16px">
+                  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                </p>
+
+              </div>
+
+              <div class="col-md-2">
+                
+              </div>
+
+              <div class="col-xs-offset-1 col-xs-10 col-md-3 col-sm-offset-1 col-sm-10">
+                
+                <h3 style="color: #7f8c8d;">Chief Executive Officer (CEO)</h3>
+                <p style="color: #7f8c8d;">Graham Glimour</p>
+
+                </br>
+
+                <h3 style="color: #7f8c8d;">Senior Executives</h3>
+                <p style="color: #7f8c8d;">Klem Richards (CTO)</p>
+                <p style="color: #7f8c8d;">Jonh Udoh (CFO)</p>
+                <p style="color: #7f8c8d;">Max Iheme (COO)</p>
+
+                </br>
+
+                <h3 style="color: #7f8c8d;">Founding Date</h3>
+                <p style="color: #7f8c8d;">June 27, 2016</p>
+
+                </br>
+
+                <h3 style="color: #7f8c8d;">Stage</h3>
+                <p style="color: #7f8c8d;">Seed</p>
+
+              </div>
+
+            </div>
+    <div class="row path8_fill path9_fill col-xs-12 col-md-12 col-sm-12">
+
+
+        <b><a href="#">Claim this page if you are the BO to verify your profile</a></b>
+
+  </div>
+    
+  </div>
+
+        </div>
+
+      </div>
+      
+     </div>
+      </div>
+    </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import Logo from '~components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style>
+
+div {
+  font-family: "Poppins", sans-serif;
+  color: #000000;
+}
+
+h1,
+h2,
+h3,
+h4 {
+  font-family: "Poppins", sans-serif;
+}
+
+a {
+  color: inherit;
+}
+a:hover, a:focus, a:active {
+  color: inherit;
+}
+
+/* =============================================================================
+  Layout
+========================================================================== */
+/* Header and Body
+================================= */
+.path0_fill {
+  color: #fff;
+  background: linear-gradient(to right, #81d76d , #80d66e, #67b788, #458cab, #2d6ec4);
+  width: 100%;
+  height: 98%;
+  position: absolute;
+}
+.logo-brand {
+  margin-top: 18% !important;
+  margin-left: 5%;
+}
+.path2_fill {
+  margin-top: -2.8%;
+  margin-left: 20%;
+  color: #fff;
+}
+.path3_fill {
+  margin-top: 3%;
+  margin-left: 15%;
+}
+.path4_fill   {
+  margin-top: 3%;
+  margin-left: 10%;
+  margin-right: 10%;
+  color: #fff;
+  font-size: 18px;
+  text-align: justify;
+  position: relative;
+}
+.path5_fill {
+  margin-top: 5%;
+  margin-bottom: 8%;
+  border-radius: 2%;
+  position: relative;
+  background-position: absolute;
+}
+.path12_fill {
+  margin-top: 10%;
+  position: relative;
+}
+.path7_fill {
+  margin-top: -1%;
+  margin-right: 10%;
+  margin-left: 10%;
+  padding-left: 5%;
+  padding-bottom: -10%;
+  width: 80%;
+  height: 50%;
+  position: relative;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  background-color: #ffffff;
+  
+}
+/* Landscape phones and down */
+
+.head_text {
+  margin-top: 10%;
+  text-align: center;
+  position: relative;
+  color: #7f8c8d;
+
+}
+.body_text {
+  margin-top: 20%;
+  position: relative;
+  color: #7f8c8d;
+}
+p {
+  text-align: justify;
+  font-size: 16px;
+}
+.path8_fill {
+  margin-top: 1%;
+  width: relative;
+  height: 5%;
+  background-color: rgba(129, 215, 110, 0.17);
+}
+.path9_fill {
+  color: #7f8c8d;
+  text-align: center;
+  font-size: 18px;
+  margin-top: 10%;
+  padding-top: 1%;
+  padding-bottom: 1%;
+}
+.path10_fill {
+  color: #7f8c8d;
+  margin-top: 5%;
+  margin-bottom: 5%;
+}
+.path11_fill{
+  margin-top: 5%;
+  margin-bottom: 5%;
+  position: relative;
+}
+</style>
